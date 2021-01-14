559c3236ccb0aece5b2ea1294080bd5036425495
ArmanMar97 <65087621+ArmanMar97@users.noreply.github.com>
2021-01-14 16:59:17 +0400

    db.config update

--- db.php
+++ db.php
@@ -4,24 +4,17 @@ const HOST = "localhost";
 const USERNAME = "root";
 const PASSWORD = "";
 
-function checkConnection($conn){
-    if ($conn){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
 //connect function is connecting to mysql server only,if particular database name is given also creating that database then connecting to it.
 function connect($dbName = null){
+    $conn = @mysqli_connect(HOST,USERNAME,PASSWORD);
     if ($dbName){
-        $conn = @mysqli_connect(HOST,USERNAME,PASSWORD);
         if ($conn){
             $sql = "CREATE DATABASE $dbName";
             $answer = mysqli_query($conn,$sql);
             if ($answer){
                 echo "New database was created and connected successfully!";
+                $conn = @mysqli_connect(HOST,USERNAME,PASSWORD,$dbName);
             }
             else{
                 echo "Error creating database " . mysqli_errno();
@@ -33,7 +26,6 @@ function connect($dbName = null){
         }
     }
     else{
-        $conn = @mysqli_connect(HOST,USERNAME,PASSWORD);
         if ($conn){
             echo "Connected to mysqli server!";
             echo "<br>";
@@ -43,20 +35,38 @@ function connect($dbName = null){
             echo "<br>";
         }
     };
+    return $conn;
 }
 
 
 function createTable($dbName = "commentProject",$tableName = "users"){
-    $conn = connect($dbName);
+    $conn =connect($dbName);
 
-    $sql = "CREATE TABLE $tableName (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+    if ($conn){
+        $sql = "CREATE TABLE $tableName (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT 'user',
+  `password` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4";
+
+        $answer = mysqli_query($conn,$sql);
+        if ($answer){
+            echo "Table created!";
+            echo "<br>";
+        }
+        else{
+            "Connection failed! " . mysqli_connect_errno();
+        }
+    }
+    else{
+        exit('Connection failed! ' . mysqli_connect_errno());
+        echo "<br>";
+    }
+
 }
-
-
 
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-include 'db.php'
+include 'db.php';
+
+
 
 ?>
 
